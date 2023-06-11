bba10b4c24995e31dacfdc0b6f456eb2a78a9951
Gridimlin <76913416+Emin141@users.noreply.github.com>
2023-06-11 18:53:08 +0200

    Adding scaffolding for the ESC button functionality

--- Source/Alkemyst/AlkemystPlayerController.cpp
+++ Source/Alkemyst/AlkemystPlayerController.cpp
@@ -2,3 +2,27 @@
 
 #include "Alkemyst/AlkemystPlayerController.h"
 
+void AAlkemystPlayerController::PushDelegeToEscapeReleased(const FKeyEventDelegate& keyEventDelegate)
+{
+	ensure(InputComponent != nullptr);
+
+	if (_keyEventStack.Num() == 0) //First time adding.
+	{
+		InputComponent->BindKey(EKeys::Escape, IE_Released, this, &AAlkemystPlayerController::OnEscapeReleased);
+	}
+	else //Push to the stack.
+	{
+		_keyEventStack.Push(keyEventDelegate);
+	}
+}
+
+void AAlkemystPlayerController::PopDelegateFromEscapeReleased()
+{
+	_keyEventStack.Pop();
+}
+
+void AAlkemystPlayerController::OnEscapeReleased()
+{
+	FKeyEventDelegate topDelegate = _keyEventStack.Pop();
+	topDelegate.ExecuteIfBound();
+}
--- Source/Alkemyst/AlkemystPlayerController.h
+++ Source/Alkemyst/AlkemystPlayerController.h
@@ -13,4 +13,26 @@ class ALKEMYST_API AAlkemystPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
+public:
+	//Declares it in the scope of the controller.
+	DECLARE_DELEGATE(FKeyEventDelegate)
+
+	//Might move this to something else later
+	void PushDelegeToEscapeReleased(const FKeyEventDelegate& keyEventDelegate);
+	void PopDelegateFromEscapeReleased();
+
+private:
+	/**
+	 * The escape key is kind of special and can have different actions bound to it
+	 * depending on the context. In order to achieve this, any class that needs to change 
+	 * the functionality of the key pushes a callable delegate to a stack of delegates.
+	 * 
+	 * When the key is then pressed/released/whatever, the top delegate is executed and 
+	 * then popped from the stack. This will allow for the previous key binding to take 
+	 * place. 
+	 */
+	TArray<FKeyEventDelegate> _keyEventStack;
+
+	void OnEscapeReleased();
+
 };
